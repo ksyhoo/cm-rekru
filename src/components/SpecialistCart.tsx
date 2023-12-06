424dc257218ffee6cd90a6ed613988adfe8a5c8e
Krzysztof Kulikowski <kulikowski.krzysztof84@gmail.com>
2023-12-06 11:03:02 +0100

    added dummy specialist components

---
+++ src/components/SpecialistCart.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SpecialistCart = () => {
+  return (
+    <div>SpecialistCart</div>
+  )
+}
+
+export default SpecialistCart
---
+++ src/components/SpecialistList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SpecialistList = () => {
+  return (
+    <div>SpecialistList</div>
+  )
+}
+
+export default SpecialistList
