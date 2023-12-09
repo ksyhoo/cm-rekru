cc0b114d0da99c3b7fd6a01fba93644dc12c7fda
Krzysztof Kulikowski <kulikowski.krzysztof84@gmail.com>
2023-12-09 11:53:28 +0100

    added bottom controls styling and content

--- src/components/SpecialistCard/BottomControls.tsx
+++ src/components/SpecialistCard/BottomControls.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  text-transform: uppercase;
+  color: #a2a8c1;
+  display: grid;
+  gap: 1px;
+  background-color: #f2f4f6;
+  grid-template-columns: 1fr 1fr;
+`;
+
+const Item = styled.div`
+  padding: 1rem;
+  font-size: 0.6rem;
+  background-color: white;
+  width: 100%;
+  text-align: center;
+`;
 
 const BottomControls = () => {
-  //TODO: implement card bottom controls
-  return <div>BottomControls</div>;
+  return (
+    <Container>
+      <Item>profile</Item>
+      <Item>book a visit</Item>
+    </Container>
+  );
 };
 
 export default BottomControls;
